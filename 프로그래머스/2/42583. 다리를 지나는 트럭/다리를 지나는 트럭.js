4bf46497dfd946ec53c68cfa8f29a560a9972d38
Lee Gyumin <82251551+legm0310@users.noreply.github.com>
2024-02-22 04:06:26 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -2,29 +2,28 @@ function solution(bridge_length, weight, truck_weights) {
   let sec = 0,
     weightSum = 0;
   const onBridge = [];
-  const truckSet = truck_weights.reduce((a, v, i) => {
-    a.push([v, bridge_length]);
-    return a;
-  }, []);
 
   while (true) {
     sec++;
     onBridge.forEach((v) => {
       if (v) v[1]--;
     });
-      
+    if(onBridge[0]) {
+
+    }
     //지나간 트럭 먼저 제거(다리의 최대무게와 현재 총 트럭무게 비교)
     if (onBridge[0]?.[1] <= 0) {
       weightSum -= onBridge[0]?.[0];
       onBridge.shift();
     }
     //트럭 --> 다리
-    if (weightSum + truckSet[0]?.[0] <= weight) {
-      weightSum += truckSet[0]?.[0];
-      onBridge.push(truckSet.shift());
+    if (weightSum + truck_weights[0] <= weight) {
+      weightSum += truck_weights[0];
+      onBridge.push([truck_weights.shift(), bridge_length]);
     }
 
-    if (truckSet.length === 0 && onBridge.length === 0) break;
+    if (truck_weights.length === 0 && onBridge.length === 0) break;
   }
+
   return sec;
 }
