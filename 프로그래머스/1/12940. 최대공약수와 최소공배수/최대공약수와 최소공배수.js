20714a5d7b8c22fbb80c1dc363fb5637aaed7cc8
Lee Gyumin <82251551+legm0310@users.noreply.github.com>
2024-02-25 22:04:20 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,11 +1,5 @@
-function solution(n, m) {
-    var answer = [1, 1];
-    for(let i=2; i<=Math.min(n, m); i++) {
-        if(n%i===0 && m%i===0) answer[0] = i
-    }
-    while(1) {
-        if(answer[1]%n===0 && answer[1]%m===0) break
-        answer[1] ++
-    }
-    return answer;
+function solution(a, b) {
+    var r;
+    for(var ab= a*b;r = a % b;a = b, b = r){}
+    return [b, ab/b];
 }
