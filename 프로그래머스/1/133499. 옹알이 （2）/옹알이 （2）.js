41ee985905dfe105802f8217a5fcc598460cce74
Lee Gyumin <82251551+legm0310@users.noreply.github.com>
2024-04-01 02:29:25 +0900

    [level 1] Title: 옹알이 (2), Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,14 @@
+function solution(babbling) {
+    const words = ["aya", "ye", "woo", "ma"]
+    return babbling.filter((v) => {
+        let temp = v
+        words.forEach((w, i) => {
+            if(temp.includes(w.repeat(2))) return
+            temp = temp.split(w).join(i)
+        })
+        for (let j=0; j<words.length; j++) {
+            temp = temp.replaceAll(`${j}`,"")
+        }
+        return temp.length===0
+    }).length
+}
