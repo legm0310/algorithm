b69ca46481bf0aa6bf8819c7bcf0f8361ce1179c
Lee Gyumin <82251551+legm0310@users.noreply.github.com>
2024-06-06 23:28:37 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 44.68 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+    for (let i = 1; i <= line; i++) {
+    console.log("*".repeat(i));
+  }
+}).on('close', function () {
+    // console.log(Number(input[0]));
+});
