6cf09a97ab92d4823f22124804945da2a01fec7f
Lee Gyumin <82251551+legm0310@users.noreply.github.com>
2024-10-05 17:12:11 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,5 @@
+function solution(date1, date2) {
+    return date1[0]<date2[0]||
+        date1[0]<=date2[0]&&date1[1]<date2[1]||
+        date1[0]<=date2[0]&&date1[1]<=date2[1]&&date1[2]<date2[2] ? 1: 0;
+}
