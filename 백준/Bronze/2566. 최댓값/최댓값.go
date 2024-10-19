fc361d101ef72f0b2551e30002ad5da27135c51a
Lee Gyumin <82251551+legm0310@users.noreply.github.com>
2024-10-20 00:13:08 +0900

    [Bronze III] Title: 최댓값, Time: 4 ms, Memory: 844 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.go
+++ 백준/Bronze/2566. 최댓값/최댓값.go
@@ -9,16 +9,15 @@ import (
 )
 
 func main() {
-	var rows int = 9
 	var result int = 0
 	maxRow := 1
 	maxCol := 1
 	reader := bufio.NewReader(os.Stdin)
-	for i := 0; i < rows; i++ {
+	for i := 0; i < 9; i++ {
 		line, _ := reader.ReadString('\n')
 		line = strings.TrimSpace(line)
 		values := strings.Fields(line)
-		for j := 0; j < len(values); j++ {
+		for j := 0; j < 9; j++ {
 			num, _ := strconv.Atoi(values[j])
 			if num > result {
 				result = num
