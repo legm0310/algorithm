dddca5e5eb6f0b588bfcea64e13d3bc4096575c1
Lee Gyumin <82251551+legm0310@users.noreply.github.com>
2025-03-13 04:21:41 +0900

    [Silver II] Title: A → B, Time: 20 ms, Memory: 12956 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var (
+	a, b int
+	s    *bufio.Scanner
+	w    *bufio.Writer
+)
+
+func init() {
+	s = bufio.NewScanner(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+	s.Split(bufio.ScanWords)
+}
+
+func scanInt() (int, error) {
+	s.Scan()
+	return strconv.Atoi(s.Text())
+}
+
+func dfs(start int) int {
+	queue := []int{start}
+	dist := make(map[int]int)
+	dist[start] = 1
+
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+		if cur == b {
+			return dist[cur]
+		}
+
+		next := []int{cur * 2, cur*10 + 1}
+		for _, n := range next {
+			if n <= b {
+				if _, exists := dist[n]; !exists {
+					dist[n] = dist[cur] + 1
+					queue = append(queue, n)
+				}
+			}
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	defer w.Flush()
+	a, _ = scanInt()
+	b, _ = scanInt()
+	w.WriteString(strconv.Itoa(dfs(a)) + "\n")
+}
