64371af805eecec47b3b3b33dc62b868cf78aabe
Lee Gyumin <82251551+legm0310@users.noreply.github.com>
2025-03-25 04:26:26 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 116 ms, Memory: 52036 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var (
+	n      int
+	parent []int
+	tree   = map[int][]int{}
+	s      = bufio.NewScanner(os.Stdin)
+	w      = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+	defer w.Flush()
+	s.Split(bufio.ScanWords)
+	n = scanInt()
+	parent = make([]int, n+1)
+	for i := 0; i < n-1; i++ {
+		a, b := scanInt(), scanInt()
+		tree[a] = append(tree[a], b)
+		tree[b] = append(tree[b], a)
+	}
+	recur(1, 0)
+	for _, i := range parent {
+		if i != 0 {
+			w.WriteString(strconv.Itoa(i) + "\n")
+		}
+	}
+}
+
+func recur(node, prev int) {
+	parent[node] = prev
+
+	for _, nxt := range tree[node] {
+		if nxt == prev {
+			continue
+		} else {
+			recur(nxt, node)
+		}
+	}
+}
+
+func scanInt() int {
+	s.Scan()
+	x, _ := strconv.Atoi(s.Text())
+	return x
+}
