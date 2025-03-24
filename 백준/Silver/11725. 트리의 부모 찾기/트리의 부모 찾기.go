fde74bd1a5682dbb527cb7453594241b4b21cc7e
Lee Gyumin <82251551+legm0310@users.noreply.github.com>
2025-03-25 04:28:33 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 60 ms, Memory: 23132 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.go
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.go
@@ -9,9 +9,10 @@ import (
 var (
 	n      int
 	parent []int
-	tree   = map[int][]int{}
-	s      = bufio.NewScanner(os.Stdin)
-	w      = bufio.NewWriter(os.Stdout)
+	//tree   = map[int][]int{}
+	tree [][]int
+	s    = bufio.NewScanner(os.Stdin)
+	w    = bufio.NewWriter(os.Stdout)
 )
 
 func main() {
@@ -19,6 +20,7 @@ func main() {
 	s.Split(bufio.ScanWords)
 	n = scanInt()
 	parent = make([]int, n+1)
+	tree = make([][]int, n+1)
 	for i := 0; i < n-1; i++ {
 		a, b := scanInt(), scanInt()
 		tree[a] = append(tree[a], b)
