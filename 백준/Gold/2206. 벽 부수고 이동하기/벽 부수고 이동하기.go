8249f2bc779fcae5e2556400f5a4ec08c9973b34
Lee Gyumin <82251551+legm0310@users.noreply.github.com>
2025-04-20 01:42:38 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 292 ms, Memory: 76736 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.go
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.go
@@ -11,12 +11,17 @@ var (
 	dx      = []int{-1, 1, 0, 0}
 	dy      = []int{0, 0, -1, 1}
 	n, m    int
-	ans     = 1 << 20
 	table   [][]int
 	visited [][][2]int
 	r       = bufio.NewReader(os.Stdin)
 	w       = bufio.NewWriter(os.Stdout)
 )
+
+type State struct {
+	x, y    int
+	isBreak int
+	dist    int
+}
 
 func main() {
 	defer w.Flush()
@@ -40,37 +45,34 @@ func main() {
 }
 
 func find(x, y int) int {
-	q := make([][]int, 0)
-	q = append(q, []int{x, y, 0, 1})
+	q := make([]*State, 0)
+	q = append(q, &State{x, y, 0, 1})
 	visited[x][y][0] = 1
+
 	for len(q) > 0 {
-		ex, ey, isBreak, dist := q[0][0], q[0][1], q[0][2], q[0][3]
+		ex, ey, isBreak, dist := q[0].x, q[0].y, q[0].isBreak, q[0].dist
 		q = q[1:]
+
 		if ex == n-1 && ey == m-1 {
 			return dist
 		}
+
 		for i := 0; i < 4; i++ {
 			nx, ny := ex+dx[i], ey+dy[i]
-			//좌표 이동 기본 조건
 			if nx >= 0 && ny >= 0 && nx < n && ny < m {
-				//for j := 0; j < 2; j++ {
-				// 1 벽인데 깬적 있을 때
-				// 2 벽인데 깬적 없을 때
-				// 3 벽이 아닐 떄
-				// 4 방문 여부
-				if table[nx][ny] == 1 && isBreak == 1 {
-					continue
-				}
-				if table[nx][ny] == 1 && isBreak == 0 {
-					if visited[nx][ny][1] == 1 {
+				if table[nx][ny] == 1 {
+					//이미 깼거나 벽을 깨고 온적 있으면 패스
+					if isBreak == 1 || visited[nx][ny][1] == 1 {
 						continue
 					}
+					//벽을 깨고 이동할 수 있는 조건 충족
 					visited[nx][ny][1] = 1
-					q = append(q, []int{nx, ny, 1, dist + 1})
-				}
-				if table[nx][ny] == 0 && visited[nx][ny][isBreak] == 0 {
-					visited[nx][ny][isBreak] = 1
-					q = append(q, []int{nx, ny, isBreak, dist + 1})
+					q = append(q, &State{nx, ny, 1, dist + 1})
+				} else {
+					if visited[nx][ny][isBreak] == 0 {
+						visited[nx][ny][isBreak] = 1
+						q = append(q, &State{nx, ny, isBreak, dist + 1})
+					}
 				}
 			}
 		}
